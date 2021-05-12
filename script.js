9e495f2239b6a66da37778a8855988df8ddfc695
andreii1907 <luchianandrei1024@yahoo.com>
2021-05-12 22:54:03 +0300

    Watch Homework

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
+const clock = document.getElementById('tic-tac');
 
 function WatchDisplay() {
     function watch(){
-        let  today = new Date();
-        let hr = today.getHours();
-        let mm = today.getMinutes(); 
-        let sc = today.getSeconds();
+        let  ourTime = new Date();
+        let hr = ourTime.getHours();
+        let mm = ourTime.getMinutes(); 
+        let sc = ourTime.getSeconds();
         if (hr < 10) hr = '0' + hr;
         if (mm < 10) mm = '0' + mm;
         if (sc < 10) sc = '0' + sc;
-        today = `${hr} : ${mm} : ${sc}`;
-        return today;
-    }
-        
-    function ticTac() {
-        const clock = document.getElementById('tic-tac');
-        console.log(clock)
-        setInterval(() => {
-            clock.textContent = watch();
-        }, 1000);
+        return ourTime = `${hr} : ${mm} : ${sc}`;
     }
 
-    return ticTac();
+    function secondWatch(){ 
+        let usTime = new Date();
+        return usTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+    }
+    
+    function firstTypeOfWatch() {
+        oneInterval = setInterval(() => {
+            clock.textContent = watch();
+        }, 0);
+    }
+
+    function secondTypeOfWatch() {
+        twoInterval = setInterval(() => {
+            clock.textContent = secondWatch();
+        }, 0);
+    }
+    
+    return {
+        firstTypeOfWatch,
+        secondTypeOfWatch
+    };
 }
 
+const twelfthHour = document.getElementById('twelfth-hour');
+const twentyFourHour = document.getElementById('twentyfour-hour');
 const watchDisplayed = new WatchDisplay();
 
+twelfthHour.addEventListener('click', function(e) {
+    e.preventDefault();
+    watchDisplayed.secondTypeOfWatch();
+    clearInterval(oneInterval);
+})
+
+twentyFourHour.addEventListener('click', function(e) {
+    e.preventDefault();
+    watchDisplayed.firstTypeOfWatch();
+    clearInterval(twoInterval);
+});
 
 
 
